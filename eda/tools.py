334dc9eeff216a856b6c438740e44f8270a152e6
Kaiziferr <kaiziferr1995@gmail.com>
2023-12-26 21:52:38 -0500

    we advancing in the exploraty analysis data

--- eda/tools.py
+++ eda/tools.py
@@ -1,5 +1,8 @@
 from unicodedata import normalize
 
+import pandas as pd
+import seaborn as sns
+
 def normalize_word(word):
   """Normaliza palabras"""
   word = word.replace(' ', '_')
@@ -19,3 +22,17 @@ def normalize_name_columns(columns):
   """Normaliza columnas"""
   columns = list(map(lambda x: normalize_word(x), columns))
   return columns
+
+def porcentaje_nulos_x_caracteristica(data:pd.DataFrame, **kwargs)->None:
+  """Calcular el porcentaje de valores nulos por caracteristicas"""
+  data.isnull().melt().pipe(
+        lambda df: (
+            sns.displot(
+                data=df,
+                y='variable',
+                hue='value',
+                multiple='fill',
+                aspect=2
+            ).set(**kwargs)
+        )
+    )
